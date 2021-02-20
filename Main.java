452cae3e5523ce3fea0087d8016f1c5926074cfd
Mandy Leung <ms.mandy@gmail.com>
2021-02-20 03:19:15 +0000

    initialization of breakout game board

--- Main.java
+++ Main.java
@@ -3,79 +3,121 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.geom.Ellipse2D;
 import java.awt.Color;
 import java.util.Random;
 import java.awt.Graphics2D;
 import java.io.*;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 class Main {
   public static void main(String[] args) {
     SwingUtilities.invokeLater(new Runnable() {
       public void run() {
-        DrawCircles f = new DrawCircles();
-        f.drawCircles();
+        Breakout f = new Breakout();
+        f.breakout();
       }
     });
   }
 }
 
-class DrawCircles extends JFrame {
-  public void drawCircles() {
-    JFrame f = new JFrame("Draw Random Circles");
+class Breakout extends JFrame {
+  public void breakout() {
+    JFrame f = new JFrame("Breakout game");
     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     DrawPanel d = new DrawPanel();
     f.add(d);
     f.pack();
     f.setVisible(true);
-
-    // for (int i = 0; i < 10; i++) {
-    //   c.generateCircle(d.getWidth(), d.getHeight());
-    // }
   }
 }
 
 class DrawPanel extends JPanel {
 
   public DrawPanel() {
-    for (int i = 0; i < 10; i++) {
-      c.generateCircle(getPreferredSize().width, getPreferredSize().height);
+    init();
+  }
+
+  /** Initialize location, color and visibility of bricks at the beginning of a game */
+  private void init() {
+    for (int i = 0; i < NBRICKS_PER_ROW; i++) {
+      for (int j = 0; j < NBRICK_ROWS; j++) {
+        xPos[i][j] = BRICK_SEP + ((BRICK_SEP + BRICK_WIDTH) * i);
+        yPos[i][j] = BRICK_Y_OFFSET + ((BRICK_SEP + BRICK_HEIGHT) * j);
+        visi[i][j] = true; 
+      }
     }
   }
 
   public Dimension getPreferredSize() {
-      return new Dimension(250,200);
+    return new Dimension(WIDTH,HEIGHT);
   }
 
   public void paintComponent(Graphics g) {
-      super.paintComponent(g);  
-      c.paintCircle(g);
+    super.paintComponent(g);  
+    for (int i = 0; i < NBRICKS_PER_ROW; i++) {
+      for (int j = 0; j < NBRICK_ROWS; j++) {
+        g.setColor(rowColor[j]);
+        g.drawRect(xPos[i][j], yPos[i][j], BRICK_WIDTH, BRICK_HEIGHT);
+      }
+    }
   }  
 
-  private Circle c = new Circle();
-
+  /** Width and height of application window in pixels */
+  public static final int APPLICATION_WIDTH = 400;
+  public static final int APPLICATION_HEIGHT = 600;
+  /** Dimensions of game board (usually the same) */
+  private static final int WIDTH = APPLICATION_WIDTH;
+  private static final int HEIGHT = APPLICATION_HEIGHT;
+  /** Dimensions of the paddle */
+  private static final int PADDLE_WIDTH = 60;
+  private static final int PADDLE_HEIGHT = 10;
+  /** Offset of the paddle up from the bottom */
+  private static final int PADDLE_Y_OFFSET = 30;
+  /** Number of bricks per row */
+  private static final int NBRICKS_PER_ROW = 10;
+  /** Number of rows of bricks */
+  private static final int NBRICK_ROWS = 10;
+  /** Separation between bricks */
+  private static final int BRICK_SEP = 4;
+  /** Width of a brick */
+  private static final int BRICK_WIDTH =
+  (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
+  /** Height of a brick */
+  private static final int BRICK_HEIGHT = 8;
+  /** Radius of the ball in pixels */
+  private static final int BALL_RADIUS = 10;
+  /** Offset of the top brick row from the top */
+  private static final int BRICK_Y_OFFSET = 70;
+  /** Number of turns */
+  private static final int NTURNS = 3;
+  /** x-coordinate of array of bricks **/
+  private static int[][] xPos = new int[NBRICKS_PER_ROW][NBRICK_ROWS];
+  /** y-coordinate of array of bricks **/
+  private static int[][] yPos = new int[NBRICKS_PER_ROW][NBRICK_ROWS];
+  /** Color of bricks by row **/
+  private static Color[] rowColor = new Color[]{Color.RED, Color.RED, Color.ORANGE, Color.ORANGE, Color.YELLOW, Color.YELLOW, Color.GREEN, Color.GREEN, Color.CYAN, Color.CYAN};
+  /** Visibility of array of bricks **/
+  private static Boolean[][] visi = new Boolean[NBRICKS_PER_ROW][NBRICK_ROWS];
 }
 
-class Circle {
+// class Circle {
   
-  public void generateCircle(int panelWidth, int panelHeight) {
-    int diameter = rand.nextInt(45) + 5;
-    int rightLimit = panelWidth-diameter;
-    int bottomLimit = panelHeight-diameter;
-    int xLoc = rand.nextInt(rightLimit);
-    int yLoc = rand.nextInt(bottomLimit);
-    circles.add(new Ellipse2D.Double(xLoc, yLoc, diameter, diameter));
-  }
+//   public void generateCircle(int panelWidth, int panelHeight) {
+//     int diameter = rand.nextInt(45) + 5;
+//     int rightLimit = panelWidth-diameter;
+//     int bottomLimit = panelHeight-diameter;
+//     int xLoc = rand.nextInt(rightLimit);
+//     int yLoc = rand.nextInt(bottomLimit);
+//     circles.add(new Ellipse2D.Double(xLoc, yLoc, diameter, diameter));
+//   }
 
-  public void paintCircle(Graphics g) {
-    Graphics2D g2d = (Graphics2D) g; 
-    for (int i = 0; i < circles.size(); i++) {
-      g2d.setPaint(new Color(rand.nextFloat(), rand.nextFloat(), rand.nextFloat()));
-      g2d.fill(circles.get(i));
-    }
-  }
+//   public void paintCircle(Graphics g) {
+//     Graphics2D g2d = (Graphics2D) g; 
+//     for (int i = 0; i < circles.size(); i++) {
+//       g2d.setPaint(new Color(rand.nextFloat(), rand.nextFloat(), rand.nextFloat()));
+//       g2d.fill(circles.get(i));
+//     }
+//   }
 
-  private Random rand = new Random();  
-  private ArrayList<Ellipse2D> circles = new ArrayList<Ellipse2D>();
-}
+//   private Random rand = new Random();  
+//   private ArrayList<Ellipse2D> circles = new ArrayList<Ellipse2D>()
